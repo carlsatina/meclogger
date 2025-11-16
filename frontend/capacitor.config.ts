1f891acc11aa8fcca56a93c6e162b6ab72b51ac3
Roderick Carl Satina <roderick.satina@procept.com.au>
2025-11-16 14:08:33 +0800

    Android capacitor has been built in android studio.

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,8 +2,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 import * as dotenv from 'dotenv';
 
 const config: CapacitorConfig = {
-    appId: 'com.rolero.quickworks',
-    appName: 'Quickworks',
+    appId: 'com.arshii.recordkeeper',
+    appName: 'RecordKeeper',
     webDir: 'dist',
     // server: {
     //     url: '192.168.2.195:8080',
